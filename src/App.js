0e1f489c85ef5aa94071cb8b705a4e72446410ae
myujjawal <myujjawal@gmail.com>
2021-12-04 21:49:53 +0530

    Basic Layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+import "./App.css";
+import Navbar from "./components/navbar";
 
 function App() {
+  const sidebarRef = useRef(null);
+  const [isResizing, setIsResizing] = useState(false);
+  const [sidebarWidth, setSidebarWidth] = useState(268);
+
+  const startResizing = React.useCallback((mouseDownEvent) => {
+    setIsResizing(true);
+  }, []);
+
+  const stopResizing = React.useCallback(() => {
+    setIsResizing(false);
+  }, []);
+
+  const resize = React.useCallback(
+    (mouseMoveEvent) => {
+      if (isResizing) {
+        setSidebarWidth(
+          mouseMoveEvent.clientX -
+            sidebarRef.current.getBoundingClientRect().left
+        );
+      }
+    },
+    [isResizing]
+  );
+
+  React.useEffect(() => {
+    window.addEventListener("mousemove", resize);
+    window.addEventListener("mouseup", stopResizing);
+    return () => {
+      window.removeEventListener("mousemove", resize);
+      window.removeEventListener("mouseup", stopResizing);
+    };
+  }, [resize, stopResizing]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app-container">
+      <Navbar />
+      <div className="main-container">
+        <div
+          ref={sidebarRef}
+          className="app-sidebar"
+          style={{ width: sidebarWidth }}
+          onMouseDown={(e) => e.preventDefault()}
         >
-          Learn React
-        </a>
-      </header>
+          <div className="app-sidebar-content">{/* <h1>Hello</h1> */}</div>
+          <div className="app-sidebar-resizer" onMouseDown={startResizing} />
+        </div>
+        <div className="app-frame">{/* <h1>World</h1> */}</div>
+      </div>
     </div>
   );
 }
